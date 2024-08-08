ac4e52bef4c95aa13aef5851952d6ceeb18df4e5
byncvn <byncvn@gmail.com>
2024-08-07 18:32:43 -0700

    adding hooks

--- src/test/java/liverpoolTests/BaseTest.java
+++ src/test/java/liverpoolTests/BaseTest.java
@@ -1,4 +1,18 @@
 package liverpoolTests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+
+
+    @BeforeMethod
+    public void launch() {
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+    }
 }
