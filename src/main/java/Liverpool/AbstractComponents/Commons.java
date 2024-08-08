9c400436129233302f556b825336be2582ed143c
byncvn <byncvn@gmail.com>
2024-08-07 18:15:13 -0700

    adding frame

--- src/main/java/Liverpool/AbstractComponents/Commons.java
+++ src/main/java/Liverpool/AbstractComponents/Commons.java
@@ -2,6 +2,9 @@ package Liverpool.AbstractComponents;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,6 +15,15 @@ public class Commons {
 
     public Commons(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
-    
+
+    @FindBy(id = "")
+    WebElement button1;
+
+    //------------------------- methods -------------------------
+
+    public void method1() {
+    }
+
 }
