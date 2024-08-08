6549f5411667da75f8c000ded6f1d8d9e7df6417
byncvn <byncvn@gmail.com>
2024-08-07 17:59:35 -0700

    adding frame

--- src/main/java/Liverpool/pageObjects/SignUpPage.java
+++ src/main/java/Liverpool/pageObjects/SignUpPage.java
@@ -1,5 +1,49 @@
 package Liverpool.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SignUpPage {
+
+    WebDriver driver;
+
+    public SignUpPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "")
+    WebElement button1;
+
+    @FindBy(id = "")
+    WebElement button2;
+
+    @FindBy(id = "")
+    WebElement button3;
+
+    @FindBy(id = "")
+    WebElement button4;
+
+    @FindBy(id = "")
+    WebElement button5;
+
+    //------------------------- methods -------------------------
+
+    public void method1() {
+    }
+
+    public void method2() {
+    }
+
+    public void method3() {
+    }
+
+    public void method4() {
+    }
+
+    public void method5() {
+    }
 
 }
--- src/main/java/Liverpool/pageObjects/categories/CategoryPage.java
+++ src/main/java/Liverpool/pageObjects/categories/CategoryPage.java
@@ -1,4 +1,48 @@
 package Liverpool.pageObjects.categories;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CategoryPage {
+
+    WebDriver driver;
+
+    public CategoryPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "")
+    WebElement button1;
+
+    @FindBy(id = "")
+    WebElement button2;
+
+    @FindBy(id = "")
+    WebElement button3;
+
+    @FindBy(id = "")
+    WebElement button4;
+
+    @FindBy(id = "")
+    WebElement button5;
+
+    //------------------------- methods -------------------------
+
+    public void method1() {
+    }
+
+    public void method2() {
+    }
+
+    public void method3() {
+    }
+
+    public void method4() {
+    }
+
+    public void method5() {
+    }
 }
