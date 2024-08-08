710c6b582190d54d2fd19dcc2994b580da020aea
byncvn <byncvn@gmail.com>
2024-08-08 12:51:26 -0700

    update methods

--- src/main/java/Liverpool/pageObjects/LoginPage.java
+++ src/main/java/Liverpool/pageObjects/LoginPage.java
@@ -17,79 +17,46 @@ public class LoginPage extends Commons {
     }
 
     @FindBy(id = "")
-    WebElement button1;
+    WebElement emailTextBox;
 
     @FindBy(id = "")
-    WebElement button2;
+    WebElement passwordTextBox;
 
     @FindBy(id = "")
-    WebElement button3;
+    WebElement forgotPasswordLink;
 
     @FindBy(id = "")
-    WebElement button4;
+    WebElement loginButton;
 
     @FindBy(id = "")
-    WebElement button5;
+    WebElement createAccountButton;
 
     @FindBy(id = "")
-    WebElement button6;
+    WebElement appleLoginButton;
 
     @FindBy(id = "")
-    WebElement button7;
+    WebElement facebookLoginButton;
 
     @FindBy(id = "")
-    WebElement button8;
+    WebElement footer;
 
     @FindBy(id = "")
-    WebElement button9;
+    WebElement header;
 
     @FindBy(id = "")
-    WebElement button10;
+    WebElement logo;
 
     @FindBy(id = "")
-    WebElement button11;
+    WebElement subBrand1;
 
     @FindBy(id = "")
-    WebElement button12;
+    WebElement informationText;
 
     @FindBy(id = "")
-    WebElement button13;
+    WebElement iniciaSesionText;
 
     @FindBy(id = "")
-    WebElement button14;
-
-    @FindBy(id = "")
-    WebElement button15;
-
-    @FindBy(id = "")
-    WebElement button16;
-
-    @FindBy(id = "")
-    WebElement button17;
-
-    @FindBy(id = "")
-    WebElement button18;
-
-    @FindBy(id = "")
-    WebElement button19;
-
-    @FindBy(id = "")
-    WebElement button20;
-
-    @FindBy(id = "")
-    WebElement button21;
-
-    @FindBy(id = "")
-    WebElement button22;
-
-    @FindBy(id = "")
-    WebElement button23;
-
-    @FindBy(id = "")
-    WebElement button24;
-
-    @FindBy(id = "")
-    WebElement button25;
+    WebElement dontHaveAccountText;
 
 
     //------------------------- methods -------------------------
@@ -98,102 +65,51 @@ public class LoginPage extends Commons {
         driver.get("https://login.liverpool.com.mx/u/login");
     }
 
-    public void clickButton1() {
-        clickElement(button1);
+    private void setEmail(String email) {
+        enterText(emailTextBox, email);
     }
 
-    public void method3() {
-        clickElement(button3);
+    private void setPassword(String password) {
+        enterText(passwordTextBox, password);
     }
 
-    public void method4() {
-        clickElement(button4);
+    private void clickLoginButton() {
+        clickElement(loginButton);
     }
 
-    public void method5() {
-        clickElement(button5);
+    public void clickCreateAccountButton() {
+        clickElement(createAccountButton);
     }
 
-    public void method6() {
-        clickElement(button6);
+    public void clickAppleLoginButton() {
+        clickElement(appleLoginButton);
     }
 
-    public void method7() {
-        clickElement(button7);
+    public void clickFacebookLoginButton() {
+        clickElement(facebookLoginButton);
     }
 
-    public void method8() {
-        clickElement(button8);
+    public void clickForgotPasswordLink() {
+        clickElement(forgotPasswordLink);
     }
 
-    public void method9() {
-        clickElement(button9);
+    public String getFooterText() {
+        return footer.getText();
     }
 
-    public void method10() {
-        clickElement(button10);
+    public String getHeaderText() {
+        return header.getText();
     }
 
-    public void method11() {
-        clickElement(button11);
+    public String getLogoText() {
+        return logo.getText();
     }
 
-    public void method12() {
-        clickElement(button12);
+    public void logIn(String email, String password){
+        setEmail(email);
+        setPassword(password);
+        clickLoginButton();
     }
-
-    public void method13() {
-        clickElement(button13);
-    }
-
-    public void method14() {
-        clickElement(button14);
-    }
-
-    public void method15() {
-        clickElement(button15);
-    }
-
-    public void method16() {
-        clickElement(button16);
-    }
-
-    public void method17() {
-        clickElement(button17);
-    }
-
-    public void method18() {
-        clickElement(button18);
-    }
-
-    public void method19() {
-        clickElement(button19);
-    }
-
-    public void method20() {
-        clickElement(button20);
-    }
-
-    public void method21() {
-        clickElement(button21);
-    }
-
-    public void method22() {
-        clickElement(button22);
-    }
-
-    public void method23() {
-        clickElement(button23);
-    }
-
-    public void method24() {
-        clickElement(button24);
-    }
-
-    public void method25() {
-        clickElement(button25);
-    }
-
 
 
 }
